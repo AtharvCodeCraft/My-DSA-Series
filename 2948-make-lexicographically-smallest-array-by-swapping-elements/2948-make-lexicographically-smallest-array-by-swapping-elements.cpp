6abbbdea2e51ee5a0e044f245e1e098ceecfae98
Atharv Patil <patilatharv2829@gmail.com>
2026-08-29 22:28:56 +0530

    Time: 178 ms (75.77%), Space: 166.1 MB (68.45%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        
+        int n = nums.size();
+
+        // Store {value, original index}
+        vector<pair<int, int>> arr;
+
+        for (int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
+        }
+
+        // Sort by value
+        sort(arr.begin(), arr.end());
+
+        vector<int> ans = nums;
+
+        int i = 0;
+
+        while (i < n) {
+
+            // Find the current group
+            int j = i;
+
+            while (j + 1 < n &&
+                   arr[j + 1].first - arr[j].first <= limit) {
+                j++;
+            }
+
+            // Get values and indices of this group
+            vector<int> values;
+            vector<int> indices;
+
+            for (int k = i; k <= j; k++) {
+                values.push_back(arr[k].first);
+                indices.push_back(arr[k].second);
+            }
+
+            // Indices must be sorted
+            sort(indices.begin(), indices.end());
+
+            // Values are already sorted
+            for (int k = 0; k < values.size(); k++) {
+                ans[indices[k]] = values[k];
+            }
+
+            i = j + 1;
+        }
+
+        return ans;
+    }
+};
